3b23484b240292140991cd5a49babd0756c55c06
sfosmark-calpoly <43476685+sfosmark-calpoly@users.noreply.github.com>
2018-09-21 08:35:06 -0700

    Update planets.py
    first code test

--- planets.py
+++ planets.py
@@ -1,7 +1,9 @@
 def weight_on_planets():
-   # write your code here
-   
-   
+   weight = int(input("What do you weigh on Earth?"))
+   marsWeight = weight * 0.38
+   jupiterWieght = weight * 2.34
+   print("\nOn Mars you would weigh {} pounds.\nOn Jupiter you would weigh {} pounds.".format(marsWeight, jupiterWeight))
+
    
 if __name__ == '__main__':
    weight_on_planets()
